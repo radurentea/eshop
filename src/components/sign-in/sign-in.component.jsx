badfe4c474af98a7b0c7f8dd13cd9d34894a9039
Radu Rentea <radu.rentea@gmail.com>
2021-01-24 18:19:07 +0200

    Create custom-buttom component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -46,7 +47,7 @@ class SignIn extends React.Component {
             handleChange={this.handleChange}
           />
 
-          <input type="submit" value="Submit form"/>
+          <CustomButton type="submit">SIGN IN</CustomButton>
         </form>
       </div>
     )
