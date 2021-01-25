a244e20fa773c31e9ea1b6fd668f5b1a300fe3bb
Radu Rentea <radu.rentea@gmail.com>
2021-01-25 11:28:46 +0200

    Add signup with email and password functionality

--- src/pages/auth/auth.component.jsx
+++ src/pages/auth/auth.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './auth.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const Auth = () => (
-  <SignIn/>
+  <div className="auth">
+    <SignIn/>
+    <SignUp/>
+  </div>
+  
 )
 
 export default Auth;
